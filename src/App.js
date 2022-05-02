57590096a1d29d1ed7b4bc13a472a3224bb85e2d
thirali0126 <thirali0126@gmail.com>
2022-05-02 10:17:34 +0530

    form commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Bloglist from './components/bloglist';
+import Blogform from './components/blogform';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path="/blogform" exact component={Blogform} />
+        <Route path="/bloglist" exact component={Bloglist}/>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/bloglist.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Bloglist() {
+  return (
+    <div>bloglist</div>
+  )
+}
+
+export default Bloglist
